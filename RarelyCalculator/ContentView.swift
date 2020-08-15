478443c7c3a66b18eeeb33bb07c05a3a132526ee
liasica <magicrolan@qq.com>
2020-08-15 19:01:26 +0800

    Initial Commit

---
+++ RarelyCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RarelyCalculator
+//
+//  Created by liasica on 2020/08/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
